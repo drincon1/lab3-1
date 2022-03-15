66e443d932435c5501dd2242549b466114ad852a
Daniel Felipe Martinez Osorio <df.martinezo@uniandes.edu.co>
2022-03-15 12:05:30 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,37 +1,20 @@
-# Este archivo contiene la representacion de los clientes
 import socket
-import os
-import threading
 
-def thread_function(i):
-	cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_address = ('localhost',10000)
-	print('[CLIENTE] connecting too {} port {}'.format(*server_address))
-	cliente.connect(server_address)
-	try:
-		message = b'This is the message. It will be repeated'
-		print(f"[CLIENTE {i}] Enviando mensaje")
-		cliente.sendall(message)
-		amount_received = 0
-		amount_expected = len(message)
+ClientSocket = socket.socket()
+host = '127.0.0.1'
+port = 1233
 
-		while(amount_received < amount_expected):
-			data = cliente.recv(16)
-			amount_received += len(data)
-			print(f'[CLIENTE {i}] Recibi {data}')
-	finally:
-		print('closing socket')
-		cliente.close()
+print('Waiting for connection')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
 
+Response = ClientSocket.recv(1024)
+while True:
+    Input = input('Say Something: ')
+    ClientSocket.send(str.encode(Input))
+    Response = ClientSocket.recv(1024)
+    print(Response.decode('utf-8'))
 
-def main():
-	#for i in range(1,3): # El máximo no es inclusivo
-	cliente_1 = threading.Thread(target=thread_function, args=(1,))
-	cliente_2 = threading.Thread(target=thread_function, args=(1,))
-	cliente_1.start()
-	cliente_2.start()
-	#print(f"[CLIENTE {i}]")
-
-
-if __name__ == "__main__":
-    main()
+ClientSocket.close()
