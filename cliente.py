d7d27d226752f78afdafbb9d07504024e3f9ba89
drincon1 <d.rincon@uniandes.edu.co>
2022-03-14 19:55:53 -0500

    Servidor ya funciona. Falta cliente, logs y pruebas

--- cliente.py
+++ cliente.py
@@ -1,23 +1,37 @@
 # Este archivo contiene la representacion de los clientes
 import socket
-import sys
+import os
+import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def thread_function(i):
+	cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_address = ('localhost',10000)
+	print('[CLIENTE] connecting too {} port {}'.format(*server_address))
+	cliente.connect(server_address)
+	try:
+		message = b'This is the message. It will be repeated'
+		print(f"[CLIENTE {i}] Enviando mensaje")
+		cliente.sendall(message)
+		amount_received = 0
+		amount_expected = len(message)
 
-server_address = ('localhost',10000)
-print('connecting too {} port {}'.format(*server_address))
-sock.connect(server_address)
-try:
-	message = b'This is the message. It will be repeated'
-	print('sending {!r}'.format(message))
-	sock.sendall(message)
-	amount_received = 0
-	amount_expected = len(message)
-	
-	while(amount_received < amount_expected):
-		data = sock.recv(16)
-		amount_received += len(data)
-		print('received {!r}'.format(data))
-finally:
-	print('closing socket')
-	sock.close()
+		while(amount_received < amount_expected):
+			data = cliente.recv(16)
+			amount_received += len(data)
+			print(f'[CLIENTE {i}] Recibi {data}')
+	finally:
+		print('closing socket')
+		cliente.close()
+
+
+def main():
+	#for i in range(1,3): # El máximo no es inclusivo
+	cliente_1 = threading.Thread(target=thread_function, args=(1,))
+	cliente_2 = threading.Thread(target=thread_function, args=(1,))
+	cliente_1.start()
+	cliente_2.start()
+	#print(f"[CLIENTE {i}]")
+
+
+if __name__ == "__main__":
+    main()
--- servidor.py
+++ servidor.py
@@ -1,32 +1,40 @@
 # Archivo .py con el codigo del servidor
 import socket
-import sys
+import os
+import threading
 
-#Creacion de un socket TCP/IP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+IP = 'localhost'
+PUERTO = 10000
 
-#Unir al socket con el puerto
-server_address = ('localhost',10000)
-print('Starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+def thread_function(connection,client_address):
+	print(f"[SERVIDOR] {client_address} conectado")
+	connection.send("Usted se ha conectado exitosamente al servidor")
+	while True:
+		data = connection.recv(16)
+		print('received{!r}'.format(data))
+		if data:
+			print('sending back to the client')
+			connection.sendall(data)
+		else:
+			print('no data from', client_address)
+			break
 
-sock.listen(1)
 
-while True:
-	print('Waiting for a conection')
-	connection, client_address = sock.accept()
-	try:
-		print('connection from', client_address)
-		
-		while True:
-			data = connection.recv(16)
-			print('received{!r}'.format(data))
-			if data:
-				print('sending back to the client')
-				connection.sendall(data)
-			else:
-				print('no data from', client_address)
-				break
-	finally:
-		connection.close()
+def main():
+	print("[SERVIDOR] INICIALIZANDO SERVIDOR")
+	servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_address = (IP,PUERTO)
+	servidor.bind(server_address)
+	servidor.listen()
+	print(f"[SERVIDOR] Servidor está escuchando en {IP}:{PUERTO}")
 
+	print("[SERVIDOR] ESPERANDO CONEXIONES")
+	while True:
+		connection, client_address = servidor.accept()
+		cliente = threading.Thread(target=thread_function, args=(connection,client_address))
+		cliente.start()
+		print(f"[CONEXIONES ACTIVAS] {threading.activeCount() - 1}")
+
+
+if __name__ == "__main__":
+    main()
