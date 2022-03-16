4906f9ddb8c80f6359c31873db13ff007c0d6abf
Daniel Rincon <d.rincon@uniandes.edu.co>
2022-03-16 11:17:11 -0500

    Commit pre-archivos
    En este commit ya funcionan servidores, clientes concurrentes, se puede mandar y guardar archivos.

--- cliente.py
+++ cliente.py
@@ -2,35 +2,48 @@
 import socket
 import os
 import threading
+# ---ATRIBUTOS---
+IP = 'localhost'
+PORT = 10000
 
-def thread_function(i):
+# ---MÉTODOS---
+def thread_function(tup):
 	cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_address = ('localhost',10000)
-	print('[CLIENTE] connecting too {} port {}'.format(*server_address))
-	cliente.connect(server_address)
+	#server_address = (IP,PORT)
+	cliente.connect(tup)
+	print(f'[CLIENTE {threading.current_thread().getName()}] conectado')
 	try:
-		message = b'This is the message. It will be repeated'
-		print(f"[CLIENTE {i}] Enviando mensaje")
-		cliente.sendall(message)
-		amount_received = 0
-		amount_expected = len(message)
+		print(f"[CLIENTE {threading.current_thread().getName()}] Esperando mensaje")
+		data = cliente.recv(16).decode("utf-8")
 
-		while(amount_received < amount_expected):
-			data = cliente.recv(16)
-			amount_received += len(data)
-			print(f'[CLIENTE {i}] Recibi {data}')
+		print(f"[CLIENTE {threading.current_thread().getName()}] El mensaje recibido fue: {data}")
+		# Cliente confirma que quiere recibir archivo
+		cliente.sendall("OK".encode("utf-8"))
+		print(f"[CLIENTE {threading.current_thread().getName()}] Esperando el archivo")
+		arch = cliente.recv(1024)
+		print(f"[CLIENTE {threading.current_thread().getName()}] Archivo recibido")
+		ruta = f"archivos_cliente/{threading.current_thread().getName()}-Prueba-0.txt"
+		file = open(ruta, "w")
+		file.write(arch.decode("utf-8"))
+		file.close()
 	finally:
 		print('closing socket')
 		cliente.close()
 
 
 def main():
-	#for i in range(1,3): # El máximo no es inclusivo
-	cliente_1 = threading.Thread(target=thread_function, args=(1,))
-	cliente_2 = threading.Thread(target=thread_function, args=(1,))
-	cliente_1.start()
-	cliente_2.start()
-	#print(f"[CLIENTE {i}]")
+	num_clientes = int(input('¿Cuántos clientes quiere conectados?\n')) + 1
+	set_up = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_address = (IP,PORT)
+	set_up.connect(server_address)
+	arch = set_up.recv(2048)
+	archivo = input('[1] 100 MB\n[2] 250 MB\n')
+	set_up.send(archivo.encode("utf-8"))
+
+
+	for i in range(1, num_clientes): # El máximo no es inclusivo
+		cliente_t = threading.Thread(target=thread_function, name="Cliente"+str(i), args=(server_address,))
+		cliente_t.start()
 
 
 if __name__ == "__main__":
--- servidor.py
+++ servidor.py
@@ -8,27 +8,43 @@ PUERTO = 10000
 
 def thread_function(connection,client_address):
 	print(f"[SERVIDOR] {client_address} conectado")
-	connection.send("Usted se ha conectado exitosamente al servidor")
-	while True:
-		data = connection.recv(16)
-		print('received{!r}'.format(data))
-		if data:
-			print('sending back to the client')
-			connection.sendall(data)
-		else:
-			print('no data from', client_address)
-			break
+	#
+	connection.send("[SERVIDOR] ¿Desea comenzar la conexión?".encode("utf-8"))
+	#while True:
+	data = connection.recv(16)
+	if data:
+		print(f'[SERVIDOR] Se ha recibido {data}')
+		# Serviro manda el archivo escogido
+		with open("archivos_servidor/prueba.txt", "r") as f:
+			text = f.read()
+		print('[SERVIDOR] Enviando el archivo')
+		connection.send(text.encode("utf-8"))
+
+	else:
+		print('no data from')
+		#break
 
 
 def main():
+
+
 	print("[SERVIDOR] INICIALIZANDO SERVIDOR")
 	servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_address = (IP,PUERTO)
-	servidor.bind(server_address)
+	try:
+		servidor.bind(server_address)
+	except servidor.error as e:
+		print(str(e))
+
+	print(f"[SERVIDOR] Escuchando en {IP}:{PUERTO}")
 	servidor.listen()
-	print(f"[SERVIDOR] Servidor está escuchando en {IP}:{PUERTO}")
 
 	print("[SERVIDOR] ESPERANDO CONEXIONES")
+	conn,set_up = servidor.accept()
+	conn.send('¿Cual archivo desea recibir sus clientes?'.encode("utf-8"))
+	archivo = conn.recv(2048)
+
+
 	while True:
 		connection, client_address = servidor.accept()
 		cliente = threading.Thread(target=thread_function, args=(connection,client_address))
