7b37a49c0dece3095959189c79682e626a909116
drincon1 <d.rincon@uniandes.edu.co>
2022-03-17 15:11:01 -0500

    Todo funciona. Faltan archivos grandes y pruebas

--- cliente.py
+++ cliente.py
@@ -2,55 +2,64 @@
 import socket
 import os
 import threading
+import hashlib
+import logging
+
 # ---ATRIBUTOS---
 IP = 'localhost'
-PORT = 10000
+PORT = 5000
 
-# ---MÉTODOS---
+#----------------------------------------------------------------
 def thread_function(tup):
 	# El cliente se conecta al servidor
 	cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	cliente.connect(tup)
-	print(f'[CLIENTE {threading.current_thread().getName()}] conectado')
+	print(f'[{threading.current_thread().getName()}] conectado')
 	try:
 		# El cliente espera que el servidor le pregunte si quiere empezar la transeferencia de archivos.
-		data = cliente.recv(16).decode("utf-8")
-		print(f"[CLIENTE {threading.current_thread().getName()}] {data}")
+		data = cliente.recv(1024).decode("utf-8")
+
+		print(f"[{threading.current_thread().getName()}] {data}")
 		# Cliente confirma que quiere recibir archivo
-		cliente.sendall("OK".encode("utf-8"))
+		cliente.send("OK".encode("utf-8"))
 		# Cliente recibe el hash del contenido del archivo
-		# TODO
-
+		hash_recv = cliente.recv(1024).decode("utf-8")
+		print(f"[{threading.current_thread().getName()}] Recibió el hash {hash_recv}")
 		#Cliente recibe el archvio
-		print(f"[CLIENTE {threading.current_thread().getName()}] Esperando el archivo")
-		arch = cliente.recv(1024)
-		print(f"[CLIENTE {threading.current_thread().getName()}] Archivo recibido")
-
+		print(f"[{threading.current_thread().getName()}] Esperando el archivo")
+		arch = ""
+		while True:
+			arch_recv = cliente.recv(2048).decode("utf-8")
+			arch += arch_recv
+			if not arch_recv:
+				break
+		#arch = cliente.recv(8).decode("utf-8")
+		print(f"[{threading.current_thread().getName()}] Archivo recibido {arch}")
 		# Se calcula el hash del archivo recibido
-		# TODO
-
+		hash_calc = hashlib.md5(arch.encode()).hexdigest().encode('utf-8')
+		hash_calc =  hash_calc.decode('utf-8')
+		print(f"[{threading.current_thread().getName()}] El hash calculado fue {hash_calc}")
 		# Si no es el mismo se informa y se acaba el proceso. Si es el mismo se guarda el archivo
-		# TODO
-		ruta = f"archivos_cliente/{threading.current_thread().getName()}-Prueba-0.txt"
-		file = open(ruta, "w")
-		file.write(arch.decode("utf-8"))
-		file.close()
+		if hash_recv == hash_calc:
+			ruta = f"archivos_cliente/{threading.current_thread().getName()}-Prueba-0.txt"
+			file = open(ruta, "w")
+			file.write(arch)
+			file.close()
+			print(f"[{threading.current_thread().getName()}] Se ha guardado el archivo")
+		else:
+			print(f"[{threading.current_thread().getName()}] ¡El archivo recibido ha sido modificado!")
 	finally:
 		print('closing socket')
 		cliente.close()
-
+#----------------------------------------------------------------
 
 def main():
-	# Cuántos clientes se manejaran
-	num_clientes = int(input('¿Cuántos clientes quiere conectados?\n')) + 1
 	# Conexión al servidor para la configuración
 	set_up = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_address = (IP,PORT)
 	set_up.connect(server_address)
-	arch = set_up.recv(2048)
-	# Escoger con cuál archivo se va a trabajar
-	archivo = input('[1] 100 MB\n[2] 250 MB\n')
-	set_up.send(archivo.encode("utf-8"))
+	# Se informa con cuántos clientes se van a trabajar
+	num_clientes = int(set_up.recv(2048).decode("utf-8"))
 
 	# Se generan la cantidad de clientes deseados de manera concurrente
 	for i in range(1, num_clientes): # El máximo no es inclusivo
--- servidor.py
+++ servidor.py
@@ -2,73 +2,93 @@
 import socket
 import os
 import threading
+import hashlib
+import logging
 
 IP = 'localhost'
-PUERTO = 10000
+PUERTO = 5000
+num_clientes = 0
+ruta_archivo = ""
 
+#----------------------------------------------------------------
 def thread_function(connection,client_address):
 	# Informa el puerto del cliente
 	print(f"[SERVIDOR] {client_address} conectado")
 	#Pregunta si desea comenzar la transferencia de archivos
 	connection.send("[SERVIDOR] ¿Desea comenzar la transferencia de archivos?".encode("utf-8"))
-
-	#Recibe la respuesta del cliente si desea o no recibir los archivos
-	data = connection.recv(16)
-	print(f'[SERVIDOR] Se ha recibido {data}')
-	if data:
-		# Se busca el archivo deseado
-		# TODO
-
-
-		with open("archivos_servidor/prueba.txt", "r") as f:
-			text = f.read()
-		# Se calcula el valor de hash al contenido del archivo
-		# TODO
-		# Se envia el hash calculado
-		# TODO
-
-		# Se espera a todos los clientes
-		# TODO
-
-		# Se envia el archivo al cliente
-		print('[SERVIDOR] Enviando el archivo')
-		# Serviro manda el archivo escogido
-		connection.send(text.encode("utf-8"))
-
-	else:
-		print('[SERVIDOR] El cliente no desea recibir archivos')
-
-
-def main():
-	# Se inicializa el servidor
-	print("[SERVIDOR] INICIALIZANDO SERVIDOR")
-	servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_address = (IP,PUERTO)
+	data = connection.recv(1024).decode("utf-8")
 	try:
-		servidor.bind(server_address)
-	except servidor.error as e:
-		print(str(e))
+		print(f'[SERVIDOR] Se ha recibido {data}')
+		if data == "OK":
+		# Se busca el archivo deseado
+			with open(ruta_archivo, "r") as f:
+				text = f.read()
+		# Se calcula el valor de hash al contenido del archivo
+			f_hash = hashlib.md5(text.encode()).hexdigest().encode('utf-8')
+			print("[SERVIDOR] El hash calculado fue",f_hash)
+		# Se envia el hash calculado
+			connection.send(f_hash)
+		# Se espera a todos los clientes
+			print(f"[SERVIDOR] {threading.current_thread().getName()} esperando")
+			barrier.wait()
+		# Se envia el archivo al cliente
+			print('[SERVIDOR] Enviando el archivo')
+		# Serviro manda el archivo escogido
+			connection.send(text.encode("utf-8"))
+		else:
+			print('[SERVIDOR] El cliente no desea recibir archivos')
+	finally:
+		connection.close()
+#----------------------------------------------------------------
 
-	# Se habilita el servidor para escuchar
-	print(f"[SERVIDOR] Escuchando en {IP}:{PUERTO}")
-	servidor.listen()
-	# CONFIGURACIÓN DEL EJERCICIO (¿Cuál archivo se transmitirá?)
-	#Se habilita el servidor para aceptar la conexión para escoger el archvio
-	print("[SERVIDOR] ESPERANDO CONEXIONES")
-	conn,set_up = servidor.accept()
-	conn.send('¿Cual archivo desea recibir sus clientes?'.encode("utf-8"))
-	archivo = conn.recv(2048)
-
-	# Ciclo infinito para siempre estar escuchando conexiones de los clientes
-	while True:
-		# Aceptar la conexión del cliente
-		connection, client_address = servidor.accept()
-		# Manejar la conexión como un hilo, i.e., concurrente
-		cliente = threading.Thread(target=thread_function, args=(connection,client_address))
-		# Comenzar el hilo <- Comunicación servidor-cliente
-		cliente.start()
-		print(f"[CONEXIONES ACTIVAS] {threading.activeCount() - 1}")
 
 
-if __name__ == "__main__":
-    main()
+# Definir con cuál archivo se trabajará
+# CONFIGURACIÓN DEL EJERCICIO (¿Cuál archivo se transmitirá?)
+res_archivo = input('¿Cual archivo desea recibir sus clientes?\n[1] 100 MB\n[2] 250 MB\n')
+#if para establecer la ruta
+if int(res_archivo) == 1:
+	ruta_archivo = "archivos_servidor/100MB.txt"
+elif int(res_archivo) == 2:
+	ruta_archivo = "archivos_servidor/250MB.txt"
+else:
+	ruta_archivo = "archivos_servidor/prueba.txt"
+
+# Cuántos clientes se manejaran
+num_clientes  = int(input('¿Cuántos clientes quiere conectados?\n')) + 1
+
+# Se inicializa el servidor
+print("[SERVIDOR] INICIALIZANDO SERVIDOR")
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (IP,PUERTO)
+try:
+	servidor.bind(server_address)
+except servidor.error as e:
+	print(str(e))
+
+# Se habilita el servidor para escuchar
+print(f"[SERVIDOR] Escuchando en {IP}:{PUERTO}")
+print("[SERVIDOR] COMIENCE LA APLICACIÓN CLIENTE.PY")
+servidor.listen()
+
+#Se habilita el servidor para hacer la configuración con la aplicación cliente
+conn,set_up = servidor.accept()
+# Enviar a la aplicación cliente cuantos clientes tendrá que simular concurrentemente
+conn.send(str(num_clientes).encode("utf-8"))
+conn.close()
+num_clientes = num_clientes - 1
+barrier = threading.Barrier(num_clientes)
+
+
+print("[SERVIDOR] ESPERANDO CONEXIONES")
+i = 1
+# Ciclo infinito para siempre estar escuchando conexiones de los clientes
+while True:
+	# Aceptar la conexión del cliente
+	connection, client_address = servidor.accept()
+	# Manejar la conexión como un hilo, i.e., concurrente
+	cliente = threading.Thread(target=thread_function, name="Cliente "+str(i),args=(connection,client_address))
+	# Comenzar el hilo <- Comunicación servidor-cliente
+	cliente.start()
+	i = i+1
+	print(f"[CONEXIONES ACTIVAS] {threading.activeCount() - 1}")
